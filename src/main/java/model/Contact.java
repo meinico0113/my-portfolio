75988e5e9063f4fbc2572458b77324bcd467570b
koyuki0113 <koyuki130104@icloud.com>
2026-02-28 15:02:21 +0900

    contact model class 作成

---
+++ src/main/java/model/Contact.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class Contact {
+
+    private int id;
+    private String category;
+    private String content;
+    private String status;
+    private Timestamp createdAt;
+
+    // ===== getter / setter =====
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
