66b3988ef815c835deb02eecbc26966f598c0afc
koyuki0113 <koyuki130104@icloud.com>
2026-03-07 12:19:11 +0900

    CategoryCreateServlet.javaの作成

---
+++ src/main/java/servlet/CategoryCreateServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import dao.CategoryDAO;
+
+@WebServlet("/admin/categoryCreate")
+public class CategoryCreateServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+
+            String name = request.getParameter("name");
+
+            // バリデーション
+            if (name == null || name.trim().length() == 0 || name.length() > 255) {
+
+                request.setAttribute("error", "カテゴリ名は255文字以内で入力してください");
+
+                request.getRequestDispatcher("/adminCategoryNew.jsp")
+                        .forward(request, response);
+
+                return;
+            }
+
+            CategoryDAO dao = new CategoryDAO();
+            dao.insert(name);
+
+            response.sendRedirect(request.getContextPath() + "/admin/categoryList");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
